d9d82f4a3b5f696753d64a1c8a0ddf39c4956030
YuckierOlive370 <urbinatrujillocesarmanuel7@gmail.com>
2026-02-03 01:29:37 -0700

    Modifique la forma de obtener la ip

--- check_status.sh
+++ check_status.sh
@@ -3,7 +3,7 @@
 echo "++++++++INFORMACION DEL S.O++++++++"
 echo "Nombre: $(hostname)"
 echo "Ip Actual: "
-hostname -I | awk '{print $1}'
+ip addr show ens37 | grep "inet " | awk '{print $2}'
 echo "Espacio actual: "
 df -h / | awk 'NR==2{print $4 " libres de " $2 " totales"}'
 echo "Fecha y hora:"
